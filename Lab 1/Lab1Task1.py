730b633ca708ab01b095fa08409e5fcfd9626cce
gavinhughes11 <g.hughes@wustl.edu>
2026-09-16 12:33:49 -0500

    Protected against all data types

--- Lab 1/Lab1Task1.py
+++ Lab 1/Lab1Task1.py
@@ -9,18 +9,18 @@ print("To calculate surface area, enter 1")
 print("To calculate volume, enter 2")
 
 # Take user input to decide calculation
-quantity = int(input("What do you want to calculate: "))
+quantity = input("What do you want to calculate: ")
 
 # Make sure user cannot enter a number other than 1 or 2
-while (quantity != 1) and (quantity != 2):
-    quantity = int(input("Error: Invalid input. Please enter 1 to calculate surface area or 2 to calculate volume: "))
+while (quantity != '1') and (quantity != '2'):
+    quantity = input("Error: Invalid input. Please enter 1 to calculate surface area or 2 to calculate volume: ")
 
 # If user chooses 1, calculate and print surface area
-if quantity == 1:
+if quantity == '1':
     surface_area = 4 * pi * radius * radius
     print("The sphere surface area is: ", surface_area)
 
 # Otherwise if user chooses 2, calculate and print volume
-elif quantity == 2:
+elif quantity == '2':
     volume = (4 * pi * radius * radius * radius)/3
     print("The sphere volume is: ", volume)
